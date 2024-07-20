15b42a906132096c3fac789f16d03c2f42fd3571
razken-hash <ka_kenniche@esi.dz>
2024-07-20 16:09:48 +0100

    Authentify Project Init

---
+++ src/test/java/app/auth/authentify/AuthentifyApplicationTests.java
@@ -0,0 +1,13 @@
+package app.auth.authentify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
